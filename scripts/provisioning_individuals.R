1ec669ab97fa95d59c13a4f5f89e04c51f08ee98
Abby Walter <lawalter@vcu.edu>
2020-06-11 15:37:56 -0400

    greek is better after all

--- scripts/provisioning_individuals.R
+++ scripts/provisioning_individuals.R
@@ -102,23 +102,23 @@ prov_prop_repeats <-
   #   repeat_m = ifelse(parent == 'ROSO', '✕', repeat_m),
   #   repeat_m2 = ifelse(parent == 'POPS', '⬟', NA)
   # )
-  mutate(
-    repeat_f = ifelse(parent == 'SUOR', '\U2B21', NA),
-    repeat_f = ifelse(parent == 'SORO', '\U25B6', repeat_f),
-    repeat_f = ifelse(parent == 'SGBY', '\U25FC', repeat_f),
-    repeat_f = ifelse(parent == 'WSPR', '\U25D1', repeat_f),
-    repeat_m = ifelse(parent == 'BUSY', '\U2605', NA),
-    repeat_m = ifelse(parent == 'ROSO', '\U2715', repeat_m),
-    repeat_m2 = ifelse(parent == 'POPS', '\U2B1F', NA)
-  )
   # mutate(
-  #   repeat_f = ifelse(parent == 'SUOR', 'α', NA),
-  #   repeat_f = ifelse(parent == 'SORO', 'ε', repeat_f),
-  #   repeat_f = ifelse(parent == 'SGBY', 'π', repeat_f),
-  #   repeat_f = ifelse(parent == 'WSPR', 'ψ', repeat_f),
-  #   repeat_m = ifelse(parent == 'BUSY', 'β', NA),
-  #   repeat_m = ifelse(parent == 'ROSO', 'γ', repeat_m),
-  #   repeat_m = ifelse(parent == 'POPS', 'δ', repeat_m))
+  #   repeat_f = ifelse(parent == 'SUOR', '\U2B21', NA),
+  #   repeat_f = ifelse(parent == 'SORO', '\U25B6', repeat_f),
+  #   repeat_f = ifelse(parent == 'SGBY', '\U25FC', repeat_f),
+  #   repeat_f = ifelse(parent == 'WSPR', '\U25D1', repeat_f),
+  #   repeat_m = ifelse(parent == 'BUSY', '\U2605', NA),
+  #   repeat_m = ifelse(parent == 'ROSO', '\U2715', repeat_m),
+  #   repeat_m2 = ifelse(parent == 'POPS', '\U2B1F', NA)
+  # )
+  mutate(
+    repeat_f = ifelse(parent == 'SUOR', 'α', NA),
+    repeat_f = ifelse(parent == 'SORO', 'ε', repeat_f),
+    repeat_f = ifelse(parent == 'SGBY', 'π', repeat_f),
+    repeat_f = ifelse(parent == 'WSPR', 'ψ', repeat_f),
+    repeat_m = ifelse(parent == 'BUSY', 'β', NA),
+    repeat_m = ifelse(parent == 'ROSO', 'γ', repeat_m),
+    repeat_m = ifelse(parent == 'POPS', 'δ', repeat_m))
 
 # Individual provisioning plot (black & white)
 ggplot(data = prov_prop_repeats, 
@@ -128,14 +128,14 @@ ggplot(data = prov_prop_repeats,
   geom_col(width = 0.75, color = 'white') +
   geom_text(
     aes(label = repeat_m), 
-    size = 4, 
-    position = position_stack(vjust = 0),
-    hjust = 'right') +
-  geom_text(
-    aes(label = repeat_m2), 
     size = 3.5, 
     position = position_stack(vjust = 0),
     hjust = 'right') +
+  # geom_text(
+  #   aes(label = repeat_m2), 
+  #   size = 3.5, 
+  #   position = position_stack(vjust = 0),
+  #   hjust = 'right') +
   geom_text(
     aes(label = repeat_f), 
     size = 3.5, 
@@ -182,10 +182,10 @@ ggplot(data = prov_prop_repeats,
                             "N1718CA2_brd1" = "T",
                             "NB18C4_18_brd1" = "U")) +
   ggsave(
-    file = "provisioning_individual_fig_bw_med.pdf",
+    file = "provisioning_individual_fig_greek.png",
     path ="plots/bw/",
     width = 3.5,
-    height = 4,
+    height = 4.5,
     units = "in",
     dpi = 600)
 
