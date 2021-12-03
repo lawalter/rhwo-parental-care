c81200740f32013111b6661b080bc327a8a296aa
Abby Walter <lawalter@mymail.vcu.edu>
2021-12-03 16:26:59 -0500

    Add files via upload

--- scripts/brooding/new_brooding_models.R
+++ scripts/brooding/new_brooding_models.R
@@ -40,6 +40,15 @@ library(bbmle)
 
 # read data ---------------------------------------------------------------
 
+# Distance and start time data 
+
+dist_start <-
+  read_csv("raw_data/provisioning_video_data.csv") %>%
+  filter(Letter == "a") %>%
+  select(
+    video_number = Video_number,
+    start_time = Start_time)
+
 # Behaviors by video
 
 behaviors <- 
@@ -47,15 +56,43 @@ behaviors <-
   as_tibble() %>%
   select(video_id, brooding_min, subject, sex, habitat, exact_age_chick,
          peeped_chick_count, nest_id, brood_id, std_jdate, tmax, 
-         usable_video) %>%
+         usable_video, video_number) %>%
   # Calculate standardized temperatures
   mutate(
     std_tmax = (tmax - mean(tmax))/sd(tmax),
-    brooding_rate = (brooding_min/usable_video)*60) 
+    brooding_rate = (brooding_min/usable_video)*60) %>% 
+  # Add video start time to dataset from previous datafame
+  left_join(dist_start, by = "video_number") %>%
+  mutate(
+    start_time =
+      case_when(
+        start_time == "na" ~ NA_character_,
+        video_number == "70" ~ "1100",
+        video_number == "117" ~ "904",
+        TRUE ~ start_time)) %>% 
+  mutate(start_time = as.numeric(start_time)) %>% 
+  mutate(brooding_min = as.integer(round(brooding_min)))
+
+
+# aside -------------------------------------------------------------------
+
+# Notes from LPB:
 # To eliminate the warnings from glmmTMB
 # "non-integer counts in a truncated_nbinom1 model"
 # Run the following mutate, but it is not needed -- it's fine as dbl
 # mutate(brooding_min = as.integer(round(brooding_min)))
+
+# Replace few missing values for video start time with the mean:
+behaviors <- 
+  transform(
+    behaviors, 
+    start_time = 
+      ifelse(
+        is.na(start_time), 
+        ave(start_time, FUN = function(x) mean(x, na.rm = TRUE)), 
+        start_time))
+
+behaviors$scTime <- scale(behaviors$start_time)
 
 # run models --------------------------------------------------------------
 
@@ -64,7 +101,8 @@ behaviors <-
 #   null model - random effects only
 null_model <- 
   glmmTMB(
-    brooding_min ~ (1|brood_id) + offset(log(usable_video)), 
+    brooding_min ~ 
+      (1|brood_id) + (1|subject) + offset(log(usable_video)), 
     data = behaviors, 
     ziformula = ~., 
     family = "truncated_nbinom1")
@@ -73,8 +111,8 @@ null_model <-
 base_model <- 
   glmmTMB(
     brooding_min ~ 
-      exact_age_chick + peeped_chick_count + 
-      (1|brood_id) + offset(log(usable_video)), 
+      exact_age_chick + peeped_chick_count + scTime +
+      (1|brood_id) + (1|subject) + offset(log(usable_video)),  
     data = behaviors, 
     ziformula = ~., 
     family = "truncated_nbinom1")
@@ -83,8 +121,8 @@ base_model <-
 model_sex <- 
   glmmTMB(
     brooding_min ~ 
-      exact_age_chick + peeped_chick_count + sex +
-      (1|brood_id) + offset(log(usable_video)), 
+      exact_age_chick + peeped_chick_count + sex + scTime +
+      (1|brood_id) + (1|subject) + offset(log(usable_video)),  
     data = behaviors, 
     ziformula = ~., 
     family = "truncated_nbinom1")
@@ -93,9 +131,8 @@ model_sex <-
 model_env <- 
   glmmTMB(
     brooding_min ~ 
-      exact_age_chick + peeped_chick_count + 
-      habitat + std_tmax +
-      (1|brood_id) + offset(log(usable_video)), 
+      exact_age_chick + peeped_chick_count + habitat + scTime + 
+      std_tmax + (1|brood_id) + (1|subject) + offset(log(usable_video)), 
     data = behaviors, 
     ziformula = ~., 
     family = "truncated_nbinom1")
@@ -104,48 +141,54 @@ model_env <-
 model_sex_env <- 
   glmmTMB(
     brooding_min ~ 
-      exact_age_chick + peeped_chick_count + 
-      sex + habitat + std_tmax +
-      (1|brood_id) + offset(log(usable_video)), 
+      exact_age_chick + peeped_chick_count + scTime+ sex + 
+      habitat + std_tmax + (1|brood_id) + (1|subject) + 
+      offset(log(usable_video)),  
     data = behaviors, 
     ziformula = ~., 
     family = "truncated_nbinom1")
 
 #   base model + Psex*chickAge interaction
-model_interaction <- 
+model_sex_interaction <- 
   glmmTMB(
     brooding_min ~ 
-      exact_age_chick*sex + peeped_chick_count + 
-      (1|brood_id) + offset(log(usable_video)), 
+      sex*exact_age_chick + scTime+
+      peeped_chick_count + (1|brood_id) + (1|subject) + 
+      offset(log(usable_video)), 
     data = behaviors, 
     ziformula = ~., 
     family = "truncated_nbinom1")
 
-#   global model: Psex*chickAge+brdSz+temp+hab
-global_model <- 
+#   global model1: Psex*chickAge+brdSz+temp+hab
+global_model_sex <- 
   glmmTMB(
     brooding_min ~ 
-      exact_age_chick*sex + peeped_chick_count + habitat + std_tmax +
-      (1|brood_id) + offset(log(usable_video)), 
+      sex*exact_age_chick + scTime+
+      peeped_chick_count + std_tmax + habitat + (1|brood_id) + (1|subject) + 
+      offset(log(usable_video)),  
     data = behaviors, 
     ziformula = ~., 
     family = "truncated_nbinom1")
+
 
 # compare models ----------------------------------------------------------
 
-ICtab(
-  null_model,
-  base_model,
-  model_sex,
-  model_env,
-  model_sex_env,
-  model_interaction,
-  global_model,
-  type = c("AIC"), 
-  weights = TRUE, delta = TRUE, base = TRUE, logLik = TRUE, sort = TRUE)
+brooding_aic <- 
+  ICtab(
+    null_model,
+    base_model,
+    model_sex,
+    model_env,
+    model_sex_env,
+    model_sex_interaction,
+    global_model_sex,
+    type = c("AICc"), 
+    weights = TRUE, delta = TRUE, base = TRUE, logLik = TRUE, sort = TRUE)
 
-summary(model_interaction) 
+write.csv(brooding_aic, "H:/brooding_aic_question.csv")
+
+summary(model_sex_interaction) 
 
 # Calculate confidence intervals
 
-confint(model_interaction, level = 0.95)
+confint(model_sex_interaction, level = 0.95)
