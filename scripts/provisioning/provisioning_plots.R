bca1d7e708b521710f0add33f088bad6b928c950
Abby Walter <lawalter@mymail.vcu.edu>
2021-12-04 15:42:41 -0500

    Add files via upload

--- scripts/provisioning/provisioning_plots.R
+++ scripts/provisioning/provisioning_plots.R
@@ -6,26 +6,67 @@ library(glmmTMB)
 
 # data --------------------------------------------------------------------
 
-bbyvid <- 
-  read.csv("clean_data/behaviors.csv", stringsAsFactors = FALSE) %>% 
+# Distance and time data 
+
+dist_start <-
+  read_csv("raw_data/provisioning_video_data.csv") %>%
+  filter(Letter == "a") %>%
+  select(
+    video_number = Video_number,
+    start_time = Start_time)
+
+# Behaviors by video
+
+behaviors <- 
+  read.csv("clean_data/behaviors.csv", stringsAsFactors = FALSE) %>%
   as_tibble() %>%
-  select(video_number, feeding = feeding_chicks, subject, brood_id, 
-         exact_age_chick, habitat, julian_date, std_jdate, usable_video, 
-         peeped_chick_count)
+  select(video_id, feeding_chicks, subject, sex, habitat, exact_age_chick,
+         peeped_chick_count, nest_id, brood_id, std_jdate, usable_video, 
+         video_number, julian_date) %>%
+  # Feeding variable
+  rename(feeding = feeding_chicks) %>%
+  # Add video start time to dataset from previous datafame
+  left_join(dist_start, by = "video_number") %>%
+  mutate(
+    start_time =
+      case_when(
+        start_time == "na" ~ NA_character_,
+        video_number == "70" ~ "1100",
+        video_number == "117" ~ "904",
+        TRUE ~ start_time)) %>% 
+  mutate(start_time = as.numeric(start_time)) %>% 
+  # Add mean value to records w/ missing start time
+  mutate(
+    start_time = 
+      ifelse(
+        is.na(start_time), 
+        ave(start_time, FUN = function(x) mean(x, na.rm = TRUE)), 
+        start_time)) 
+
+behaviors$scTime <- scale(behaviors$start_time)
 
 # script ------------------------------------------------------------------
 
 # Top GLMM
-model4.1x2 <- glmmTMB(feeding ~ I(exact_age_chick^2)*std_jdate + peeped_chick_count + exact_age_chick + (1|brood_id) + (1|subject)  + offset(log(usable_video)), data = bbyvid, ziformula = ~1, family = nbinom2(link = "log"))
+model_date_interaction <-   
+  glmmTMB(
+    feeding ~ 
+      std_jdate*I(exact_age_chick^2) + 
+      std_jdate*exact_age_chick + 
+      peeped_chick_count + scTime + (1|brood_id) + (1|subject) + 
+      offset(log(usable_video)), 
+    data = behaviors, 
+    ziformula = ~1, 
+    family = nbinom2(link = "log"))
 
 # Generate dataframes with needed model variables to generate prediction
 # https://github.com/glmmTMB/glmmTMB/issues/378
 
 # Remove duplicates because there is no significant difference bw sexes
-preddata <- bbyvid[!duplicated(bbyvid$video_number), ]
+preddata <- behaviors[!duplicated(behaviors$video_number), ]
 
 # Predict feeding rates
-initial <- predict(model4.1x2, preddata, type = "response")
+initial <- predict(model_date_interaction, preddata, type = "response")
 
 # Calculate rates 
 preddata <-
@@ -43,103 +84,35 @@ preddata <-
 
 # plots -------------------------------------------------------------------
 
-# Colors
-colors_sex <- c("female" = "#F47C89", "male" = "#7b758e")
-colors_hab <- c("CC" = "#76d6b0", "Savanna" = "#FFB84D")
-colors_dat <- c("Early summer" = "#6bd2db", "Late summer" = "#fc913a")
-
-# plots -------------------------------------------------------------------
-
-# Brood size prov per hr
-ggplot(preddata, 
-       aes(exact_age_chick, predper_hr), 
-       color=as.factor(peeped_chick_count), 
-       fill=as.factor(peeped_chick_count)) +
-  stat_smooth(method = glm, formula = y ~ x + I(x^2), 
-              aes(y = predper_hr, color=as.factor(peeped_chick_count), 
-                  fill=as.factor(peeped_chick_count)), alpha = 0.2,
-              se = TRUE, level = 0.95, fullrange = TRUE) +
-  labs(x = "Chick age (day)", 
-       y = "Predicted provisioning \n (per hour)") + 
-  guides(color=guide_legend("Brood Size"), fill = FALSE) +
-  theme_classic() +
-  theme(text=element_text(size = 12, family="mono"), 
-        legend.position = "bottom") 
-
-## Overall prov/hr
-ggplot(preddata, aes(exact_age_chick, predper_hr)) +
-  stat_smooth(method = glm, formula = y ~ x + I(x^2), 
-              aes(y = predper_hr, color="deeppink4", 
-                  fill="deeppink4"), alpha = 0.2,
-              se = TRUE, level = 0.95, fullrange = TRUE) +
-  labs(x = "Chick age (day)", 
-       y = "Predicted provisioning (per hour)") +
-  theme_classic() +
-  theme(text=element_text(size = 12, family="mono"), 
-        legend.position = "none") 
-
-# Overall prov/chk/hr
-ggplot(preddata, aes(exact_age_chick, predper_chkhr)) +
-  stat_smooth(method = glm, formula = y ~ x + I(x^2), 
-              aes(y = predper_chkhr, color="deeppink4", 
-                  fill="deeppink4"), alpha = 0.2,
-              se = TRUE, level = 0.95, fullrange = TRUE) +
-  labs(x = "Chick age (day)", 
-       y = "Predicted provisioning \n (per chick/hour)") +
-  theme_classic() +
-  theme(text=element_text(size = 12, family="mono"), 
-        legend.position = "none") 
-
-# Predicted provisioning rate by date (color)
-ggplot(preddata, aes(exact_age_chick, predper_chkhr)) +
-  stat_smooth(method = glm, formula = y ~ x + I(x^2), 
-              aes(y = predper_chkhr, color=Date, 
-                  fill=Date), alpha = 0.2,
-              se = TRUE, level = 0.95, fullrange = TRUE) +
-  labs(title = "Figure 4",
-       x = "Chick age (day)", 
-       y = "Predicted provisioning \n (per chick/hour)") + 
-  guides(color=guide_legend("Date")) +
-  scale_fill_manual(values = colors_dat) +
-  scale_color_manual(values = colors_dat) +
-  theme_classic() +
-  theme(axis.title.x = element_text(size = 10), 
-        axis.title.y = element_text(size = 10),
-        axis.text.y = element_text(size = 9),
-        axis.text.x = element_text(size = 9),
-        legend.text = element_text(size = 10),
-        legend.title = element_text(size = 10),
-        legend.position = "bottom") 
-  # ggsave(
-  #   file = "provisioning_fig_color.png",
-  #   path ="plots/color/",
-  #   width = 3.5,
-  #   height = 3,
-  #   units = "in",
-  #   dpi = 600)
-
 # Predicted provisioning rate by date (black & white)
-ggplot(
-  preddata, 
-  aes(exact_age_chick, predper_chkhr)) +
+
+age_plot <- 
+  ggplot(
+    preddata, 
+    aes(
+      x = exact_age_chick, 
+      y = predper_chkhr)) +
   stat_smooth(
     method = glm, 
     formula = y ~ x + I(x^2), 
-    aes(y = predper_chkhr, color = Date, fill = Date, linetype = Date), 
+    aes(y = predper_chkhr
+        #color = Date, fill = Date, linetype = Date
+        ), 
     size = 0.5, 
+    color = "black",
     se = TRUE, 
     level = 0.95,
     fullrange = TRUE) +
   labs(
-    #title = "Figure 4",
-       x = "Chick age (day)", 
-       y = "Predicted provisioning \n (per chick per hr)") + 
-  guides(color = guide_legend("Date")) +
+    title = "Figure 4",
+    x = "Chick age (day)", 
+    y = "Predicted provisioning \n (per chick per hr)") + 
+  #guides(color = guide_legend("Date")) +
   scale_y_continuous(limits = c(0, 4.5)) +
-  scale_linetype_discrete(labels = c("early summer", "late summer")) +
-  scale_shape_manual(values = c(17, 1), labels = c("early summer", "late summer")) +
-  scale_color_manual(values = c("Early summer" = "black", "Late summer" = "black"), labels = c("early summer", "late summer")) +
-  scale_fill_grey(start = 0.6, end = 0.6, labels = c("early summer", "late summer")) +
+  # scale_linetype_discrete(labels = c("early summer", "late summer")) +
+  # scale_shape_manual(values = c(17, 1), labels = c("early summer", "late summer")) +
+  # scale_color_manual(values = c("Early summer" = "black", "Late summer" = "black"), labels = c("early summer", "late summer")) +
+  # scale_fill_grey(start = 0.6, end = 0.6, labels = c("early summer", "late summer")) +
   theme_classic() +
   theme(axis.title.x = element_text(size = 10), 
         axis.title.y = element_text(size = 10),
@@ -147,15 +120,40 @@ ggplot(
         axis.text.x = element_text(size = 9),
         legend.text = element_text(size = 10),
         legend.title = element_text(size = 10),
-        legend.position = "bottom") +
+        legend.position = "bottom") 
+
+# Predicted provisioning rate by brood size
+
+broodsize_plot <- 
+  ggplot(
+    preddata, 
+    aes(
+      x = as.factor(peeped_chick_count), 
+      y = predper_hr)) +
+  geom_violin(lwd = 0.5) +
+  labs(x = "Brood size", 
+       y = "Predicted provisioning (per hr)") +
+  theme_classic() +
+  theme(axis.title.x = element_text(size = 10), 
+        axis.title.y = element_text(size = 10),
+        axis.text.y = element_text(size = 9, color = 'black'),
+        axis.text.x = element_text(size = 9, color = 'black'),
+        legend.text = element_text(size = 10),
+        legend.title = element_text(size = 10),
+        legend.position = "none")
+
+
+
+cowplot::plot_grid(age_plot, broodsize_plot, 
+                   labels = 'AUTO',
+                   nrow = 2, ncol = 1) +
   ggsave(
-    file = "fig4_provisioning.tiff",
+    file = "fig4_provisioning.png",
     path ="plots/manuscript_plots/",
     width = 3.5,
-    height = 3,
+    height = 7,
     units = "in",
     dpi = 300)
-
 
 # comparison plot ---------------------------------------------------------
 
